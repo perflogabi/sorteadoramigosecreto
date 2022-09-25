0f32cc48d6d5e7d26b58fc2955c3b607b554e483
Gabriela Florêncio <perflogabi@gmail.com>
2022-09-24 23:39:52 -0300

    quinto commit

---
+++ src/componentes/Rodape.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom"
+import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes"
+
+const Rodape = () => {
+
+    const participantes = useListaDeParticipantes()
+
+    const navegarPara = useNavigate()
+
+    const iniciar = () => {
+        navegarPara('/sorteio')
+    }
+
+    return (<footer className="rodape-configuracoes">
+        <button className="botao" disabled={participantes.length <3} onClick={iniciar}>Iniciar brincadeira</button>
+        <img src="/imagens/sacolas.png" alt="Sacolas de compras" />
+    </footer>)
+}
+
+export default Rodape
